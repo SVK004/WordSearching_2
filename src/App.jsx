152c8818bfc472d224466b6ec0ad8127d99f73e5
AmazingCuber <saivenkatkrishna2004@gmail.com>
2024-07-24 14:56:44 +0530

    language select

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,51 @@
-// import './App.css'
-// import PixiComponent from '../src/components/PixiComponent'
-import Home from '../src/components/Home'
-import PixiGame1 from './components/PixiGame1'
-import PixiGame2 from '../src/components/PixiGame2'
- import PixiGame3 from '../src/components/PixiGame3'
-// import PixiComponent4 from '../src/components/PixiGame4'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import PixiGame4 from '../src/components/PixiGame4'
-import PixiGame5 from'../src/components/PixiGame5'
-import PixiGame6 from '../src/components/PixiGame6'
-import PixiGame7 from '../src/components/PixiGame7'
-import PixiGame8 from '../src/components/PixiGame8'
-import PixiGame9 from '../src/components/PixiGame9'
-import PixiGame10 from '../src/components/PixiGame10'
-import Completed from '../src/components/Completed'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../src/components/Home';
+import PixiGame1 from './components/PixiGame1';
+import PixiGame2 from '../src/components/PixiGame2';
+import PixiGame3 from '../src/components/PixiGame3';
+import PixiGame4 from '../src/components/PixiGame4';
+import PixiGame5 from'../src/components/PixiGame5';
+import PixiGame6 from '../src/components/PixiGame6';
+import PixiGame7 from '../src/components/PixiGame7';
+import PixiGame8 from '../src/components/PixiGame8';
+import PixiGame9 from '../src/components/PixiGame9';
+import PixiGame10 from '../src/components/PixiGame10';
+import Completed from '../src/components/Completed';
 
 function App() {
+  const [lang, setLang] = useState('English'); // default language is English
+
+  // useEffect(() => {
+  //   const handlePointerDown = () => {
+  //     console.log(lang);
+  //   };
+
+  //   window.addEventListener('pointerdown', handlePointerDown);
+
+  //   return () => {
+  //     window.removeEventListener('pointerdown', handlePointerDown);
+  //   };
+  // }, [lang]);
+
   return (
     <Router basename="/games/wordsearching">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/game1" element={<PixiGame1 />} />
-        <Route path="/game2" element={<PixiGame2 />} />
-        <Route path="/game3" element={<PixiGame3 />} />
-        <Route path="/game4" element={<PixiGame4 />} />
-        <Route path="/game5" element={<PixiGame5 />} />
-        <Route path="/game6" element={<PixiGame6 />} />
-        <Route path="/game7" element={<PixiGame7 />} />
-        <Route path="/game8" element={<PixiGame8 />} />
-        <Route path="/game9" element={<PixiGame9 />} />
-        <Route path="/game10" element={<PixiGame10 />} />
+        <Route path="/" element={<Home lang={lang} setLang={setLang} />} />
+        <Route path="/game1" element={<PixiGame1 lang={lang} />} />
+        <Route path="/game2" element={<PixiGame2 lang={lang} />} />
+        <Route path="/game3" element={<PixiGame3 lang={lang} />} />
+        <Route path="/game4" element={<PixiGame4 lang={lang} />} />
+        <Route path="/game5" element={<PixiGame5 lang={lang} />} />
+        <Route path="/game6" element={<PixiGame6 lang={lang} />} />
+        <Route path="/game7" element={<PixiGame7 lang={lang} />} />
+        <Route path="/game8" element={<PixiGame8 lang={lang} />} />
+        <Route path="/game9" element={<PixiGame9 lang={lang} />} />
+        <Route path="/game10" element={<PixiGame10 lang={lang} />} />
         <Route path="/Completed" element={<Completed />} />
       </Routes>
     </Router>
   );
 }
+
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,61 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
 
-function Home() {
+function Home(prop) {
+
+  let lang = prop.lang;
+  let setLang = prop.setLang
+  const handleLangChange = (event) => {
+    setLang(event.target.value);
+  };
+
   return (
     <>
-    <div className="App1">
-      <div className="header-container">
-        <h1 className='head1'>Word Search</h1>
-        <img src="../src/assets/images/word_search icon.png" alt="Search Icon"  className='image'/>
+      <div className="App1">
+        <div className="header-container">
+          <h1 className='head1'>Word Search</h1>
+          <img src="../src/assets/images/word_search icon.png" alt="Search Icon" className='image' />
+        </div>
+        <h3>Welcome to the Puzzle Game</h3>
+        <span>
+          <Link to="/game1">
+            <button className="Start-button">Start</button>
+          </Link>
+          <Link to="https://joywithlearning.com/games">
+            <button className="Home-button">Home</button>
+          </Link>
+        </span>
+        <br/>
+        <div>
+          <input 
+            type='radio' 
+            id='English' 
+            name='Language' 
+            value='English' 
+            checked={lang === 'English'} 
+            onChange={handleLangChange} 
+          />
+          <label htmlFor='English'>English</label>
+          <input 
+            type='radio' 
+            id='Hindi' 
+            name='Language' 
+            value='Hindi' 
+            checked={lang === 'Hindi'} 
+            onChange={handleLangChange} 
+          />
+          <label htmlFor='Hindi'>Hindi</label>
+          <input 
+            type='radio' 
+            id='Telugu' 
+            name='Language' 
+            value='Telugu' 
+            checked={lang === 'Telugu'} 
+            onChange={handleLangChange} 
+          />
+          <label htmlFor='Telugu'>Telugu</label>
+        </div>
       </div>
-      <h3>Welcome to the Puzzle Game</h3>
-      <span>
-      <Link to="/game1">
-        <button className="Start-button">Start</button>
-      </Link>
-      <Link to="https://joywithlearning.com/games ">
-        <button className="Home-button">Home</button>
-      </Link>
-      </span>
-    </div>
     </>
   );
 }
